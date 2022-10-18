6b52809a14e6cd439423e909680546c2bb5f1450
MichaelSeitz98 <65539604+MichaelSeitz98@users.noreply.github.com>
2022-10-19 00:50:20 +0200

    Anpassung Kommentare

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -1,4 +1,4 @@
-#based on https://github.com/datitran/raccoon_dataset/blob/master/generate_tfrecord.py
+# Basierend auf: https://github.com/datitran/raccoon_dataset/blob/master/generate_tfrecord.py
 
 from __future__ import division
 from __future__ import print_function
